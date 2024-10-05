10029f441694f20dd31a2d658ed7f88628beb16c
Samlegamer <samlegamer.yt@gmail.com>
2024-10-05 21:34:30 +0200

    start sajevius addons

---
+++ Sajevius/1.16.5/src/main/java/fr/samlegamer/mcwsajevius/MappingsFix.java
@@ -0,0 +1,29 @@
+package fr.samlegamer.mcwsajevius;
+
+import fr.samlegamer.addonslib.mapping.MappingMissing;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = McwSajevius.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class MappingsFix
+{
+	private static final MappingMissing.Bridges bridges_betterlands = new MappingMissing.Bridges("mcwbridgesaurora", McwSajevius.MODID, McwSajevius.wood_betterlands, McwSajevius.stone_betterlands);
+	private static final MappingMissing.Bridges bridges_shroomed = new MappingMissing.Bridges("mcwbridgesaurora", McwSajevius.MODID, McwSajevius.wood_shroomed);
+	
+	@SubscribeEvent
+	public static void missingnoWoodBlock(RegistryEvent.MissingMappings<Block> event)
+	{
+		bridges_betterlands.missingnoWoodBlock(event);
+		bridges_shroomed.missingnoWoodBlock(event);
+	}
+	
+	@SubscribeEvent
+	public static void missingnoWoodItem(RegistryEvent.MissingMappings<Item> event)
+	{
+		bridges_betterlands.missingnoWoodItem(event);
+		bridges_shroomed.missingnoWoodItem(event);
+	}
+}
---
+++ Sajevius/1.16.5/src/main/java/fr/samlegamer/mcwsajevius/McwSajevius.java
@@ -0,0 +1,128 @@
+package fr.samlegamer.mcwsajevius;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import fr.samlegamer.addonslib.Finder;
+import fr.samlegamer.addonslib.Registration;
+import fr.samlegamer.addonslib.bridges.Bridges;
+import fr.samlegamer.addonslib.fences.Fences;
+import fr.samlegamer.addonslib.furnitures.Furnitures;
+import fr.samlegamer.addonslib.roofs.Roofs;
+import fr.samlegamer.addonslib.tab.NewIconRandom;
+
+@Mod(McwSajevius.MODID)
+public class McwSajevius
+{    
+	public static final String MODID = "mcwsajevius";
+	private static final Logger LOGGER = LogManager.getLogger();
+
+	public static final List<String> wood_shroomed = Arrays.asList("blue_hard_mushroom", "hard_mushroom", "orange_hard_mushroom", "purple_hard_mushroom");
+	public static final List<String> wood_betterlands = Arrays.asList("juniper");
+	public static final List<String> stone_betterlands = Arrays.asList("black_terracotta_bricks",
+	"blue_terracotta_bricks",
+	"brown_terracotta_bricks",
+	"claystone_bricks",
+	"cyan_terracotta_bricks",
+	"gray_terracotta_bricks",
+	"green_terracotta_bricks",
+	"light_blue_terracotta_bricks",
+	"light_gray_terracotta_bricks",
+	"lime_terracotta_bricks",
+	"magenta_terracotta_bricks",
+	"orange_terracotta_bricks",
+	"red_terracotta_bricks",
+	"terracotta_bricks",
+	"white_terracotta_bricks",
+	"yellow_terracotta_bricks");
+	
+    private static final DeferredRegister<Block> block = Registration.blocks(MODID);
+    private static final DeferredRegister<Item> item = Registration.items(MODID);
+
+    public static final ItemGroup MCWSAJEVIUS_TAB = new ItemGroup(MODID + ".tab") {
+	    @Override
+	    public ItemStack makeIcon() {
+	        return new ItemStack(new NewIconRandom.Properties(Finder.findBlock(MODID, randomNaming()+"_roof"), Finder.findBlock(MODID, randomNaming()+"_picket_fence"), Finder.findBlock(MODID, randomNaming()+"_wardrobe"), 
+	        	    Finder.findBlock(MODID, randomNaming()+"_log_bridge_middle"), Blocks.CRAFTING_TABLE, Blocks.CRAFTING_TABLE, Blocks.CRAFTING_TABLE, Blocks.CRAFTING_TABLE).bridges().fences().furnitures().roofs().buildWood());
+	    }
+	};
+
+    public McwSajevius()
+    {
+    	LOGGER.info("Macaw's Sajevius Mod Loading...");
+    	Registration.init(block, item);
+
+    	final AbstractBlock.Properties wood = AbstractBlock.Properties.of(Material.WOOD).harvestTool(ToolType.AXE).strength(0.5F, 2.5F).sound(SoundType.WOOD);
+    	final AbstractBlock.Properties leaves = AbstractBlock.Properties.copy(Blocks.OAK_LEAVES);
+    	final AbstractBlock.Properties stone = AbstractBlock.Properties.of(Material.STONE).harvestTool(ToolType.PICKAXE).strength(3.0F, 5.0F).sound(SoundType.STONE);
+
+    	Bridges.setRegistrationWoodModLoaded(wood_shroomed, block, item, MCWSAJEVIUS_TAB, "shroomed", wood);
+    	Roofs.setRegistrationWoodModLoaded(wood_shroomed, block, item, MCWSAJEVIUS_TAB, "shroomed", wood);
+    	Fences.setRegistrationWoodModLoaded(wood_shroomed, block, item, MCWSAJEVIUS_TAB, "shroomed", wood);
+    	Furnitures.setRegistrationWoodModLoaded(wood_shroomed, block, item, MCWSAJEVIUS_TAB, "shroomed");
+    	
+    	Bridges.setRegistrationWoodModLoaded(wood_betterlands, block, item, MCWSAJEVIUS_TAB, "betterlands", wood);
+    	Roofs.setRegistrationWoodModLoaded(wood_betterlands, block, item, MCWSAJEVIUS_TAB, "betterlands", wood);
+    	Fences.setRegistrationWoodModLoaded(wood_betterlands, block, item, MCWSAJEVIUS_TAB, "betterlands", wood);
+    	Furnitures.setRegistrationWoodModLoaded(wood_betterlands, block, item, MCWSAJEVIUS_TAB, "betterlands");
+    	Fences.setRegistrationHedgesModLoaded(wood_betterlands, block, item, MCWSAJEVIUS_TAB, "betterlands", leaves);
+
+    	Bridges.setRegistrationRockModLoaded(stone_betterlands, block, item, MCWSAJEVIUS_TAB, "betterlands", stone);
+    	Roofs.setRegistrationRockModLoaded(stone_betterlands, block, item, MCWSAJEVIUS_TAB, "betterlands", stone);
+    	Fences.setRegistrationRockModLoaded(stone_betterlands, block, item, MCWSAJEVIUS_TAB, "betterlands", stone);
+    	
+    	FMLJavaModLoadingContext.get().getModEventBus().addListener(Bridges::setupClient);
+    	FMLJavaModLoadingContext.get().getModEventBus().addListener(Roofs::setupClient);
+    	FMLJavaModLoadingContext.get().getModEventBus().addListener(Fences::setupClient);
+    	FMLJavaModLoadingContext.get().getModEventBus().addListener(Furnitures::setupClient);
+        MinecraftForge.EVENT_BUS.register(MappingsFix.class);
+    	LOGGER.info("Macaw's Sajevius Mod Finish !");
+    }
+    
+	private static String randomNaming()
+	{
+		Random rand = new Random();
+
+		if(ModList.get().isLoaded("shroomed") && ModList.get().isLoaded("betterlands"))
+		{
+			int i = rand.nextInt(1);
+
+			switch(i)
+			{
+			case 0:
+					return "juniper";
+			case 1:
+					return "purple_hard_mushroom";
+			}
+		}
+		else
+		{
+			if(ModList.get().isLoaded("shroomed"))
+			{
+				return "purple_hard_mushroom";
+			}
+
+			if(ModList.get().isLoaded("betterlands"))
+			{
+				return "juniper";
+			}
+		}
+		return "purple_hard_mushroom";
+	}
+}
