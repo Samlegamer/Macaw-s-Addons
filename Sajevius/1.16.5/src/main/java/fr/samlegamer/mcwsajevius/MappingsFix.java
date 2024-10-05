caccf834c747bc818eff4561dd371a09492c31b8
Samlegamer <samlegamer.yt@gmail.com>
2024-10-05 21:54:37 +0200

    fix convertor

--- Sajevius/1.16.5/src/main/java/fr/samlegamer/mcwsajevius/MappingsFix.java
+++ Sajevius/1.16.5/src/main/java/fr/samlegamer/mcwsajevius/MappingsFix.java
@@ -10,13 +10,14 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = McwSajevius.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class MappingsFix
 {
-	private static final MappingMissing.Bridges bridges_betterlands = new MappingMissing.Bridges("mcwbridgesaurora", McwSajevius.MODID, McwSajevius.wood_betterlands, McwSajevius.stone_betterlands);
-	private static final MappingMissing.Bridges bridges_shroomed = new MappingMissing.Bridges("mcwbridgesaurora", McwSajevius.MODID, McwSajevius.wood_shroomed);
+	private static final MappingMissing.Bridges bridges_betterlands = new MappingMissing.Bridges("mcwbridgessajevius", McwSajevius.MODID, McwSajevius.wood_betterlands, McwSajevius.stone_betterlands);
+	private static final MappingMissing.Bridges bridges_shroomed = new MappingMissing.Bridges("mcwbridgessajevius", McwSajevius.MODID, McwSajevius.wood_shroomed);
 	
 	@SubscribeEvent
 	public static void missingnoWoodBlock(RegistryEvent.MissingMappings<Block> event)
 	{
 		bridges_betterlands.missingnoWoodBlock(event);
+		bridges_betterlands.missingnoStoneBlock(event, false);
 		bridges_shroomed.missingnoWoodBlock(event);
 	}
 	
@@ -24,6 +25,7 @@ public class MappingsFix
 	public static void missingnoWoodItem(RegistryEvent.MissingMappings<Item> event)
 	{
 		bridges_betterlands.missingnoWoodItem(event);
+		bridges_betterlands.missingnoStoneItem(event, false);
 		bridges_shroomed.missingnoWoodItem(event);
 	}
 }
