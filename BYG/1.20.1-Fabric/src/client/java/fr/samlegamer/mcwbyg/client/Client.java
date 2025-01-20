25badc1fd1e200c99f4be61bf109102b4f8b03e0
Samlegamer <samlegamer.yt@gmail.com>
2025-01-20 17:12:48 +0100

    port 1.20.1 fabric

--- BYG/1.20.1-Fabric/src/client/java/fr/samlegamer/mcwbyg/client/Client.java
+++ BYG/1.20.1-Fabric/src/client/java/fr/samlegamer/mcwbyg/client/Client.java
@@ -11,7 +11,7 @@ import net.fabricmc.api.Environment;
 @Environment(EnvType.CLIENT)
 public class Client implements ClientModInitializer
 {
-	private static final List<String> LEAVES_NO_COLORED = List.of("flowering_yucca", "mahogany");
+	private static final List<String> LEAVES_NO_COLORED = List.of("maple", "flowering_yucca", "mahogany");
 	private static final ColorRegistry COLOR = new ColorRegistry(McwByg.MODID, LEAVES_NO_COLORED);
 
 	@Override
