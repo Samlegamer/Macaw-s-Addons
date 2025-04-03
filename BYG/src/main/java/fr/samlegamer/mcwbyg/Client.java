ae2feca7c74084dc260d8855a718ed7fe5eb4b50
Samlegamer <samlegamer.yt@gmail.com>
2025-04-03 23:17:04 +0200

    port 1.18.2 to Fences 1.2.0

--- BYG/src/main/java/fr/samlegamer/mcwbyg/Client.java
+++ BYG/src/main/java/fr/samlegamer/mcwbyg/Client.java
@@ -1,29 +1,47 @@
 package fr.samlegamer.mcwbyg;
 
-import java.util.Arrays;
 import java.util.List;
-import fr.samlegamer.addonslib.client.ColorRegistry;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ColorHandlerEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
-@Mod.EventBusSubscriber(modid = McwByg.MODID, bus = Bus.MOD, value = Dist.CLIENT)
-public class Client
+import fr.samlegamer.addonslib.bridges.Bridges;
+import fr.samlegamer.addonslib.client.ColorRegistry;
+import fr.samlegamer.addonslib.door.Doors;
+import fr.samlegamer.addonslib.fences.Fences;
+import fr.samlegamer.addonslib.furnitures.Furnitures;
+import fr.samlegamer.addonslib.path.Paths;
+import fr.samlegamer.addonslib.roofs.Roofs;
+import fr.samlegamer.addonslib.stairs.Stairs;
+import fr.samlegamer.addonslib.trapdoor.Trapdoors;
+import fr.samlegamer.addonslib.windows.Windows;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class Client implements ClientModInitializer
 {
-	private static final List<String> LEAVES_NO_COLORED = Arrays.asList("mahogany", "maple");
+	private static final List<String> LEAVES_NO_COLORED = List.of("mahogany", "maple");
 	private static final ColorRegistry COLOR = new ColorRegistry(McwByg.MODID, LEAVES_NO_COLORED);
 
-	@SubscribeEvent
-	public static void colorsBlock(ColorHandlerEvent.Block event)
+	@Override
+	public void onInitializeClient()
 	{
-		COLOR.colorsBlock(event);
-	}
-	
-	@SubscribeEvent
-	public static void colorsItem(ColorHandlerEvent.Item event)
-	{
-		COLOR.colorsItem(event);
+		COLOR.colorsBlock();
+		COLOR.colorsItem();
+		
+    	Bridges.clientWood(McwByg.MODID, McwByg.WOOD);
+    	Bridges.clientStone(McwByg.MODID, McwByg.bridges_rockable);
+    	Roofs.clientWood(McwByg.MODID, McwByg.WOOD);
+    	Roofs.clientStone(McwByg.MODID, McwByg.fences_rockable);
+    	Fences.clientWood(McwByg.MODID, McwByg.WOOD);
+    	Fences.clientHedge(McwByg.MODID, McwByg.LEAVES);
+    	Fences.clientStone(McwByg.MODID, McwByg.fences_rockable);
+    	Furnitures.clientWood(McwByg.MODID, McwByg.WOOD);
+    	Stairs.clientWood(McwByg.MODID, McwByg.WOOD);
+
+		// 1.1 update
+		Doors.clientWood(McwByg.MODID, McwByg.WOOD);
+		Trapdoors.clientWood(McwByg.MODID, McwByg.WOOD);
+		Paths.clientWood(McwByg.MODID, McwByg.WOOD);
+		Windows.clientWood(McwByg.MODID, McwByg.WOOD);
 	}
 }
--- BYG/src/main/java/fr/samlegamer/mcwbyg/Mapping.java
+++ BYG/src/main/java/fr/samlegamer/mcwbyg/Mapping.java
@@ -1,14 +1,7 @@
 package fr.samlegamer.mcwbyg;
 
 import fr.samlegamer.addonslib.mapping.MappingMissing;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
-@Mod.EventBusSubscriber(modid = McwByg.MODID, bus = Bus.MOD)
 public class Mapping
 {
 	private static final MappingMissing.Bridges bridges = new MappingMissing.Bridges("macawsbridgesbyg", McwByg.MODID, McwByg.WOOD, McwByg.bridges_rockable);
@@ -16,27 +9,14 @@ public class Mapping
 	private static final MappingMissing.Fences fences = new MappingMissing.Fences("mcwfencesbyg", McwByg.MODID, McwByg.WOOD, McwByg.fences_rockable);
 	private static final MappingMissing.Roofs roofs = new MappingMissing.Roofs("macawsroofsbyg", McwByg.MODID, McwByg.WOOD);
 	
-	@SubscribeEvent
-	public static void missingnoWoodBlock(RegistryEvent.MissingMappings<Block> event)
+	public static void missingnoWoodBlock()
 	{
 		fences.leavesAdding(McwByg.LEAVES);
-		bridges.missingnoWoodBlock(event);
-		bridges.missingnoStoneBlock(event, true);
-		furnitures.missingnoWoodBlock(event);
-		fences.missingnoWoodBlock(event);
-		fences.missingnoStoneBlock(event);
-		roofs.missingnoWoodBlock(event);
-	}
-
-	@SubscribeEvent
-	public static void missingnoWoodItem(RegistryEvent.MissingMappings<Item> event)
-	{
-		fences.leavesAdding(McwByg.LEAVES);
-		bridges.missingnoWoodItem(event);
-		bridges.missingnoStoneItem(event, true);
-		furnitures.missingnoWoodItem(event);
-		fences.missingnoWoodItem(event);
-		fences.missingnoStoneItem(event);
-		roofs.missingnoWoodItem(event);
+		bridges.missingnoWoodBlock();
+		bridges.missingnoStoneBlock(true);
+		furnitures.missingnoWoodBlock();
+		fences.missingnoWoodBlock();
+		fences.missingnoStoneBlock();
+		roofs.missingnoWoodBlock();
 	}
 }
