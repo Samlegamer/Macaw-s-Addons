c224457614a7ba4cb73abcc65be561f20ce4839e
Samlegamer <samlegamer.yt@gmail.com>
2025-04-07 11:33:14 +0200

    bop 1.20.1 forge update fences and addonslib

--- BOP/1.20.1/src/main/java/fr/samlegamer/mcwbiomesoplenty/McwBOP.java
+++ BOP/1.20.1/src/main/java/fr/samlegamer/mcwbiomesoplenty/McwBOP.java
@@ -1,5 +1,6 @@
 package fr.samlegamer.mcwbiomesoplenty;
 
+import fr.samlegamer.addonslib.tab.NewIconRandom;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
@@ -7,20 +8,14 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import fr.samlegamer.addonslib.Finder;
 import fr.samlegamer.addonslib.Registration;
 import fr.samlegamer.addonslib.bridges.Bridges;
@@ -49,52 +44,56 @@ public class McwBOP
     public static final DeferredRegister<CreativeModeTab> ct = Registration.creativeModeTab(McwBOP.MODID);
 
 	public static final RegistryObject<CreativeModeTab> MCWBOP_TAB = ct.register("tab", () -> CreativeModeTab.builder()
-	        .icon(() -> { return new ItemStack(getIcon()); }).title(Component.translatable(McwBOP.MODID+".tab")).build());
+	        .icon(McwBOP::getIcon).title(Component.translatable(McwBOP.MODID+".tab")).build());
 
     public McwBOP()
     {
     	LOGGER.info("Macaw's Biomes O' Plenty Loading...");
      	Registration.init(block, item, ct);
-    	Bridges.setRegistrationWood(WOOD, block, item, null);
-    	Fences.setRegistrationWood(WOOD, block, item, null);
-    	Fences.setRegistrationHedges(LEAVES, block, item, null);
-    	Furnitures.setRegistrationWood(WOOD, block, item, null);
-    	Roofs.setRegistrationWood(WOOD, block, item, null);
-    	Trapdoors.setRegistrationWood(WOOD, block, item, null);
-    	Paths.setRegistrationWood(WOOD, block, item, null);
-    	Doors.setRegistrationWood(WOOD, block, item, null);
-    	Windows.setRegistrationWood(WOOD, block, item, null);
-    	Stairs.setRegistrationWood(WOOD, block, item, null);
+    	Bridges.setRegistrationWood(WOOD, block, item);
+    	Fences.setRegistrationWood(WOOD, block, item);
+    	Fences.setRegistrationHedges(LEAVES, block, item);
+    	Furnitures.setRegistrationWood(WOOD, block, item);
+    	Roofs.setRegistrationWood(WOOD, block, item);
+    	Trapdoors.setRegistrationWood(WOOD, block, item);
+    	Paths.setRegistrationWood(WOOD, block, item);
+    	Doors.setRegistrationWood(WOOD, block, item);
+    	Windows.setRegistrationWood(WOOD, block, item);
+    	Stairs.setRegistrationWood(WOOD, block, item);
 
-    	FMLJavaModLoadingContext.get().getModEventBus().addListener(this::addTotab);
+    	FMLJavaModLoadingContext.get().getModEventBus().addListener(this::addToTab);
 		MinecraftForge.EVENT_BUS.register(Mapping.class);
     	LOGGER.info("Macaw's Biomes O' Plenty Is Charged !");
     }
-    
-    private static Block getIcon()
-    {
-    	List<String> listMod = List.of("mcwroofs", "mcwfences", "mcwbridges", "mcwfurnitures", "mcwwindows", "mcwdoors", "mcwtrpdoors", "mcwpaths", "mcwstairs");
-    	int nbDepedencies = 0;
-    	List<Block> blockBase = List.of(Finder.findBlock(McwBOP.MODID, "dead_roof"), Finder.findBlock(McwBOP.MODID, "dead_picket_fence"), Finder.findBlock(McwBOP.MODID, "dead_wardrobe"), 
-    	        Finder.findBlock(McwBOP.MODID, "dead_log_bridge_middle"), Finder.findBlock(McwBOP.MODID, "dead_window"), Finder.findBlock(McwBOP.MODID, "dead_japanese_door"), Finder.findBlock(McwBOP.MODID, "dead_glass_trapdoor"), 
-    	        Finder.findBlock(McwBOP.MODID, "dead_planks_path"), Finder.findBlock(McwBOP.MODID, "dead_loft_stairs"));
-    	List<Block> blockFinal = new ArrayList<Block>();
 
-    	for(int i = 0; i < listMod.size(); i++)
-    	{
-    		if(ModList.get().isLoaded(listMod.get(i)))
-    		{
-    			nbDepedencies=nbDepedencies+1;
-    			blockFinal.add(blockBase.get(i));
-    		}
-    	}
-    	
-		Random rnd = new Random();
-		int i = rnd.nextInt(blockFinal.size()-1);
-		return blockFinal.get(i);
+	private static ItemStack getIcon()
+    {
+		NewIconRandom.NewProperties prop = new NewIconRandom.NewProperties(
+				Finder.findBlock(MODID, "redwood_roof"),
+				Finder.findBlock(MODID, "redwood_picket_fence"),
+				Finder.findBlock(MODID, "redwood_wardrobe"),
+				Finder.findBlock(MODID, "redwood_log_bridge_middle"),
+				Finder.findBlock(MODID, "redwood_pane_window"),
+				Finder.findBlock(MODID, "redwood_modern_door"),
+				Finder.findBlock(MODID, "redwood_mystic_trapdoor"),
+				Finder.findBlock(MODID, "redwood_planks_path"),
+				Finder.findBlock(MODID, "redwood_skyline_stairs"));
+
+		prop.addType(NewIconRandom.BlockType.BRIDGES)
+				.addType(NewIconRandom.BlockType.ROOFS)
+				.addType(NewIconRandom.BlockType.FENCES)
+				.addType(NewIconRandom.BlockType.FURNITURES)
+				.addType(NewIconRandom.BlockType.STAIRS)
+				.addType(NewIconRandom.BlockType.PATHS)
+				.addType(NewIconRandom.BlockType.WINDOWS)
+				.addType(NewIconRandom.BlockType.DOORS)
+				.addType(NewIconRandom.BlockType.TRAPDOORS);
+		Block icon = prop.buildIcon(NewIconRandom.BlockType.BRIDGES, NewIconRandom.BlockType.ROOFS, NewIconRandom.BlockType.FENCES, NewIconRandom.BlockType.FURNITURES, NewIconRandom.BlockType.STAIRS,
+				NewIconRandom.BlockType.PATHS, NewIconRandom.BlockType.WINDOWS, NewIconRandom.BlockType.DOORS, NewIconRandom.BlockType.TRAPDOORS);
+		return new ItemStack(icon);
     }
     
-    private void addTotab(BuildCreativeModeTabContentsEvent event)
+    private void addToTab(BuildCreativeModeTabContentsEvent event)
     {
     	if(MCWBOP_TAB != null)
     	{
