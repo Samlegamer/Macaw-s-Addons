52c6b829c01d3c706f27fe4110c452d161500b13
Samlegamer <83614614+Samlegamer@users.noreply.github.com>
2025-12-17 22:39:39 +0100

    make port to BOP Fabric 1.20.1 for 1.5

--- BOP/1.20.1-Fabric/src/client/java/fr/samlegamer/mcwbiomesoplenty/client/Client.java
+++ BOP/1.20.1-Fabric/src/client/java/fr/samlegamer/mcwbiomesoplenty/client/Client.java
@@ -1,8 +1,8 @@
 package fr.samlegamer.mcwbiomesoplenty.client;
 
 import java.util.Arrays;
-import java.util.List;
-
+import fr.addonslib.api.client.McwColors;
+import fr.samlegamer.addonslib.Finder;
 import fr.samlegamer.addonslib.Registration;
 import fr.samlegamer.addonslib.client.APIRenderTypes;
 import fr.samlegamer.addonslib.client.ColorRegistry;
@@ -14,14 +14,19 @@ import net.fabricmc.api.Environment;
 @Environment(EnvType.CLIENT)
 public class Client implements ClientModInitializer
 {
-	private static final List<String> LEAVES_NO_COLORED = Arrays.asList("pine", "mahogany", "willow", "palm", "flowering_oak");
-	private static final ColorRegistry COLOR = new ColorRegistry(McwBOP.MODID, LEAVES_NO_COLORED);
+	private static final ColorRegistry COLOR = new ColorRegistry(new McwColors(Arrays.asList(
+			Finder.makeIdHedge(McwBOP.MODID, "pine"),
+			Finder.makeIdHedge(McwBOP.MODID, "mahogany"),
+			Finder.makeIdHedge(McwBOP.MODID, "willow"),
+			Finder.makeIdHedge(McwBOP.MODID, "palm"),
+			Finder.makeIdHedge(McwBOP.MODID, "flowering_oak")
+	)));
 
 	@Override
 	public void onInitializeClient()
 	{
-		COLOR.colorsBlock();
-		COLOR.colorsItem();
+		COLOR.registryBlockColorsAverage();
+		COLOR.registryItemColors();
         APIRenderTypes.initAllWood(McwBOP.MODID, McwBOP.WOOD, Registration.getAllModTypeWood());
         APIRenderTypes.initAllLeave(McwBOP.MODID, McwBOP.LEAVES);
 	}
--- BOP/1.20.1-Fabric/src/main/java/fr/samlegamer/mcwbiomesoplenty/BlockLootTables.java
+++ BOP/1.20.1-Fabric/src/main/java/fr/samlegamer/mcwbiomesoplenty/BlockLootTables.java
@@ -2,9 +2,6 @@ package fr.samlegamer.mcwbiomesoplenty;
 
 import fr.samlegamer.addonslib.generation.loot_tables.McwLootTables;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.minecraft.registry.RegistryWrapper;
-
-import java.util.concurrent.CompletableFuture;
 
 public class BlockLootTables extends McwLootTables
 {
