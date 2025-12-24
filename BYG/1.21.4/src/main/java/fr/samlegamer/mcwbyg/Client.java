2ed9ff0cc5121b9184c7917d3e30152919b61238
Samlegamer <83614614+Samlegamer@users.noreply.github.com>
2025-12-24 20:50:27 +0100

    BYG 1.21.4 forge port for 1.5

--- BYG/1.21.4/src/main/java/fr/samlegamer/mcwbyg/Client.java
+++ BYG/1.21.4/src/main/java/fr/samlegamer/mcwbyg/Client.java
@@ -1,7 +1,9 @@
 package fr.samlegamer.mcwbyg;
 
 import java.util.Arrays;
-import java.util.List;
+
+import fr.addonslib.api.client.McwColors;
+import fr.samlegamer.addonslib.Finder;
 import fr.samlegamer.addonslib.client.ColorRegistry;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterColorHandlersEvent;
@@ -12,12 +14,13 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 @Mod.EventBusSubscriber(modid = McwByg.MODID, bus = Bus.MOD, value = Dist.CLIENT)
 public class Client
 {
-	private static final List<String> LEAVES_NO_COLORED = Arrays.asList("mahogany", "maple");
-	private static final ColorRegistry COLOR = new ColorRegistry(McwByg.MODID, LEAVES_NO_COLORED);
+	private static final ColorRegistry COLOR = new ColorRegistry(new McwColors(Arrays.asList(
+			Finder.makeIdHedge(McwByg.MODID, "mahogany"),
+			Finder.makeIdHedge(McwByg.MODID, "maple"))));
 
 	@SubscribeEvent
 	public static void colorsBlock(RegisterColorHandlersEvent.Block event)
 	{
-		COLOR.colorsBlock(event);
+		COLOR.registryBlockColorsAverage(event);
 	}
 }
