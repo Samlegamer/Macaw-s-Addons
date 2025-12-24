e3d2e92f5df0a768f63424c97cf24f263bd04e89
Samlegamer <83614614+Samlegamer@users.noreply.github.com>
2025-12-24 23:07:20 +0100

    BOP 1.21.4 Neoforge for 1.5

--- BOP/1.21.4-Neoforge/src/main/java/fr/samlegamer/mcwbiomesoplenty/Client.java
+++ BOP/1.21.4-Neoforge/src/main/java/fr/samlegamer/mcwbiomesoplenty/Client.java
@@ -1,6 +1,8 @@
 package fr.samlegamer.mcwbiomesoplenty;
 
-import java.util.List;
+import java.util.Arrays;
+import fr.addonslib.api.client.McwColors;
+import fr.samlegamer.addonslib.Finder;
 import fr.samlegamer.addonslib.client.ColorRegistry;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -10,12 +12,17 @@ import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
 @EventBusSubscriber(modid = McwBOP.MODID, value = Dist.CLIENT)
 public class Client
 {
-	private static final List<String> LEAVES_NO_COLORED = List.of("pine", "mahogany", "willow", "palm", "flowering_oak");
-	private static final ColorRegistry COLOR = new ColorRegistry(McwBOP.MODID, LEAVES_NO_COLORED);
+	private static final ColorRegistry COLOR = new ColorRegistry(new McwColors(Arrays.asList(
+			Finder.makeIdHedge(McwBOP.MODID, "pine"),
+			Finder.makeIdHedge(McwBOP.MODID, "mahogany"),
+			Finder.makeIdHedge(McwBOP.MODID, "willow"),
+			Finder.makeIdHedge(McwBOP.MODID, "palm"),
+			Finder.makeIdHedge(McwBOP.MODID, "flowering_oak")
+	)));
 
 	@SubscribeEvent
 	public static void colorsBlock(RegisterColorHandlersEvent.Block event)
 	{
-		COLOR.colorsBlock(event);
+		COLOR.registryBlockColorsAverage(event);
 	}
 }
