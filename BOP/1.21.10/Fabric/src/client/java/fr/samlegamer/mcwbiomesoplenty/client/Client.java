6155455fa04cc1a29cf59c7b88481d3b95cfb24b
Samlegamer <83614614+Samlegamer@users.noreply.github.com>
2025-12-27 14:50:51 +0100

    BOP Fabric 1.21.10 port for 1.5

--- BOP/1.21.10/Fabric/src/client/java/fr/samlegamer/mcwbiomesoplenty/client/Client.java
+++ BOP/1.21.10/Fabric/src/client/java/fr/samlegamer/mcwbiomesoplenty/client/Client.java
@@ -1,7 +1,7 @@
 package fr.samlegamer.mcwbiomesoplenty.client;
 
 import java.util.Arrays;
-import java.util.List;
+import fr.addonslib.api.client.McwColors;
 import fr.samlegamer.addonslib.Finder;
 import fr.samlegamer.addonslib.Registration;
 import fr.samlegamer.addonslib.client.APIRenderTypes;
@@ -16,13 +16,18 @@ import net.minecraft.client.color.world.BiomeColors;
 @Environment(EnvType.CLIENT)
 public class Client implements ClientModInitializer
 {
-	private static final List<String> LEAVES_NO_COLORED = Arrays.asList("pine", "mahogany", "willow", "palm", "flowering_oak");
-	private static final ColorRegistry COLOR = new ColorRegistry(McwBOP.MODID, LEAVES_NO_COLORED);
+	private static final ColorRegistry COLOR = new ColorRegistry(new McwColors(Arrays.asList(
+			Finder.makeIdHedge(McwBOP.MODID, "pine"),
+			Finder.makeIdHedge(McwBOP.MODID, "mahogany"),
+			Finder.makeIdHedge(McwBOP.MODID, "willow"),
+			Finder.makeIdHedge(McwBOP.MODID, "palm"),
+			Finder.makeIdHedge(McwBOP.MODID, "flowering_oak")
+	)));
 
 	@Override
 	public void onInitializeClient()
 	{
-		COLOR.colorsBlock();
+		COLOR.registryBlockColorsAverage();
 		ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) ->
 				(world != null && pos != null) ? BiomeColors.getDryFoliageColor(world, pos) : -10732494, Finder.findBlock(McwBOP.MODID, "dead_hedge"));
 		APIRenderTypes.initAllWood(McwBOP.MODID, McwBOP.WOOD, Registration.getAllModTypeWood());
--- BOP/1.21.10/Fabric/src/main/java/fr/samlegamer/mcwbiomesoplenty/Recipes.java
+++ BOP/1.21.10/Fabric/src/main/java/fr/samlegamer/mcwbiomesoplenty/Recipes.java
@@ -1,10 +1,9 @@
 package fr.samlegamer.mcwbiomesoplenty;
 
 import biomesoplenty.core.BiomesOPlenty;
+import fr.addonslib.api.recipes.material.McwWoodMat;
 import fr.samlegamer.addonslib.generation.recipes.McwRecipes;
-import fr.samlegamer.addonslib.generation.recipes.mat.McwWoodMat;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.block.Block;
 import net.minecraft.data.recipe.RecipeExporter;
 import net.minecraft.data.recipe.RecipeGenerator;
@@ -15,7 +14,7 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import static biomesoplenty.api.block.BOPBlocks.*;
 
-public class Recipes extends FabricRecipeProvider
+public class Recipes extends McwRecipes
 {
     public Recipes(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
         super(output, registriesFuture);
@@ -30,29 +29,28 @@ public class Recipes extends FabricRecipeProvider
                         PALM_LEAVES, REDWOOD_LEAVES, UMBRAN_LEAVES, WILLOW_LEAVES, EMPYREAL_LEAVES, PINE_LEAVES, ORANGE_MAPLE_LEAVES,
                         RED_MAPLE_LEAVES, YELLOW_MAPLE_LEAVES, CYPRESS_LEAVES, SNOWBLOSSOM_LEAVES, FLOWERING_OAK_LEAVES, ORIGIN_OAK_LEAVES);
 
-                List<McwWoodMat> woodMats = getWoodMats();
-                McwRecipes.registerAllMcwWood(this, recipeExporter, McwBOP.MODID, BiomesOPlenty.MOD_ID, McwBOP.WOOD, woodMats);
-                McwRecipes.registerMcwHedge(this, recipeExporter, McwBOP.MODID, BiomesOPlenty.MOD_ID, McwBOP.LEAVES, leave);
+                List<McwWoodMat<Block>> woodMats = getWoodMats();
+                registerAllMcwWood(this, recipeExporter, McwBOP.MODID, BiomesOPlenty.MOD_ID, McwBOP.WOOD, woodMats);
+                registerMcwHedge(this, recipeExporter, McwBOP.MODID, BiomesOPlenty.MOD_ID, McwBOP.LEAVES, leave);
             }
         };
     }
 
-    private List<McwWoodMat> getWoodMats()
-    {
-        List<McwWoodMat> woodMats = new ArrayList<>();
-        woodMats.add(new McwWoodMat(DEAD_LOG, DEAD_PLANKS, STRIPPED_DEAD_LOG, DEAD_SLAB, DEAD_FENCE, DEAD_TRAPDOOR));
-        woodMats.add(new McwWoodMat(FIR_LOG, FIR_PLANKS, STRIPPED_FIR_LOG, FIR_SLAB, FIR_FENCE, FIR_TRAPDOOR));
-        woodMats.add(new McwWoodMat(HELLBARK_LOG, HELLBARK_PLANKS, STRIPPED_HELLBARK_LOG, HELLBARK_SLAB, HELLBARK_FENCE, HELLBARK_TRAPDOOR));
-        woodMats.add(new McwWoodMat(JACARANDA_LOG, JACARANDA_PLANKS, STRIPPED_JACARANDA_LOG, JACARANDA_SLAB, JACARANDA_FENCE, JACARANDA_TRAPDOOR));
-        woodMats.add(new McwWoodMat(MAGIC_LOG, MAGIC_PLANKS, STRIPPED_MAGIC_LOG, MAGIC_SLAB, MAGIC_FENCE, MAGIC_TRAPDOOR));
-        woodMats.add(new McwWoodMat(MAHOGANY_LOG, MAHOGANY_PLANKS, STRIPPED_MAHOGANY_LOG, MAHOGANY_SLAB, MAHOGANY_FENCE, MAHOGANY_TRAPDOOR));
-        woodMats.add(new McwWoodMat(PALM_LOG, PALM_PLANKS, STRIPPED_PALM_LOG, PALM_SLAB, PALM_FENCE, PALM_TRAPDOOR));
-        woodMats.add(new McwWoodMat(REDWOOD_LOG, REDWOOD_PLANKS, STRIPPED_REDWOOD_LOG, REDWOOD_SLAB, REDWOOD_FENCE, REDWOOD_TRAPDOOR));
-        woodMats.add(new McwWoodMat(UMBRAN_LOG, UMBRAN_PLANKS, STRIPPED_UMBRAN_LOG, UMBRAN_SLAB, UMBRAN_FENCE, UMBRAN_TRAPDOOR));
-        woodMats.add(new McwWoodMat(WILLOW_LOG, WILLOW_PLANKS, STRIPPED_WILLOW_LOG, WILLOW_SLAB, WILLOW_FENCE, WILLOW_TRAPDOOR));
-        woodMats.add(new McwWoodMat(EMPYREAL_LOG, EMPYREAL_PLANKS, STRIPPED_EMPYREAL_LOG, EMPYREAL_SLAB, EMPYREAL_FENCE, EMPYREAL_TRAPDOOR));
-        woodMats.add(new McwWoodMat(MAPLE_LOG, MAPLE_PLANKS, STRIPPED_MAPLE_LOG, MAPLE_SLAB, MAPLE_FENCE, MAPLE_TRAPDOOR));
-        woodMats.add(new McwWoodMat(PINE_LOG, PINE_PLANKS, STRIPPED_PINE_LOG, PINE_SLAB, PINE_FENCE, PINE_TRAPDOOR));
+    private List<McwWoodMat<Block>> getWoodMats() {
+        List<McwWoodMat<Block>> woodMats = new ArrayList<>();
+        woodMats.add(new McwWoodMat<>(DEAD_LOG, DEAD_PLANKS, STRIPPED_DEAD_LOG, DEAD_SLAB, DEAD_FENCE, DEAD_TRAPDOOR));
+        woodMats.add(new McwWoodMat<>(FIR_LOG, FIR_PLANKS, STRIPPED_FIR_LOG, FIR_SLAB, FIR_FENCE, FIR_TRAPDOOR));
+        woodMats.add(new McwWoodMat<>(HELLBARK_LOG, HELLBARK_PLANKS, STRIPPED_HELLBARK_LOG, HELLBARK_SLAB, HELLBARK_FENCE, HELLBARK_TRAPDOOR));
+        woodMats.add(new McwWoodMat<>(JACARANDA_LOG, JACARANDA_PLANKS, STRIPPED_JACARANDA_LOG, JACARANDA_SLAB, JACARANDA_FENCE, JACARANDA_TRAPDOOR));
+        woodMats.add(new McwWoodMat<>(MAGIC_LOG, MAGIC_PLANKS, STRIPPED_MAGIC_LOG, MAGIC_SLAB, MAGIC_FENCE, MAGIC_TRAPDOOR));
+        woodMats.add(new McwWoodMat<>(MAHOGANY_LOG, MAHOGANY_PLANKS, STRIPPED_MAHOGANY_LOG, MAHOGANY_SLAB, MAHOGANY_FENCE, MAHOGANY_TRAPDOOR));
+        woodMats.add(new McwWoodMat<>(PALM_LOG, PALM_PLANKS, STRIPPED_PALM_LOG, PALM_SLAB, PALM_FENCE, PALM_TRAPDOOR));
+        woodMats.add(new McwWoodMat<>(REDWOOD_LOG, REDWOOD_PLANKS, STRIPPED_REDWOOD_LOG, REDWOOD_SLAB, REDWOOD_FENCE, REDWOOD_TRAPDOOR));
+        woodMats.add(new McwWoodMat<>(UMBRAN_LOG, UMBRAN_PLANKS, STRIPPED_UMBRAN_LOG, UMBRAN_SLAB, UMBRAN_FENCE, UMBRAN_TRAPDOOR));
+        woodMats.add(new McwWoodMat<>(WILLOW_LOG, WILLOW_PLANKS, STRIPPED_WILLOW_LOG, WILLOW_SLAB, WILLOW_FENCE, WILLOW_TRAPDOOR));
+        woodMats.add(new McwWoodMat<>(EMPYREAL_LOG, EMPYREAL_PLANKS, STRIPPED_EMPYREAL_LOG, EMPYREAL_SLAB, EMPYREAL_FENCE, EMPYREAL_TRAPDOOR));
+        woodMats.add(new McwWoodMat<>(MAPLE_LOG, MAPLE_PLANKS, STRIPPED_MAPLE_LOG, MAPLE_SLAB, MAPLE_FENCE, MAPLE_TRAPDOOR));
+        woodMats.add(new McwWoodMat<>(PINE_LOG, PINE_PLANKS, STRIPPED_PINE_LOG, PINE_SLAB, PINE_FENCE, PINE_TRAPDOOR));
         return woodMats;
     }
 
