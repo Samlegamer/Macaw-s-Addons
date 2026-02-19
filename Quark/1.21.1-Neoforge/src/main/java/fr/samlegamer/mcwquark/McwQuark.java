96fabee1a25cf9d40ac44d114c51d0346b0bb4f9
Samlegamer <83614614+Samlegamer@users.noreply.github.com>
2026-02-20 00:07:34 +0100

    Quark 1.21.1 Neoforge port

---
+++ Quark/1.21.1-Neoforge/src/main/java/fr/samlegamer/mcwquark/McwQuark.java
@@ -0,0 +1,170 @@
+package fr.samlegamer.mcwquark;
+
+import fr.addonslib.api.data.ModType;
+import fr.samlegamer.addonslib.Registration;
+import fr.samlegamer.addonslib.client.APIRenderTypes;
+import fr.samlegamer.addonslib.generation.loot_tables.McwLootTables;
+import fr.samlegamer.addonslib.generation.tags.McwBlockTags;
+import fr.samlegamer.addonslib.generation.tags.McwItemTags;
+import fr.samlegamer.addonslib.registry.McwRegistry;
+import fr.samlegamer.addonslib.tab.APICreativeTab;
+import fr.samlegamer.addonslib.util.McwMod;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import fr.samlegamer.addonslib.Finder;
+import fr.samlegamer.addonslib.tab.NewIconRandom;
+import org.jetbrains.annotations.NotNull;
+import javax.annotation.Nonnull;
+
+@Mod(McwQuark.MODID)
+public class McwQuark extends McwMod
+{
+	public static final String MODID = "mcwquark";
+	private static final Logger LOGGER = LogManager.getLogger();
+
+    public static final List<String> stone = List.of("soul_sandstone_bricks", "limestone_bricks", "jasper_bricks", "shale_bricks", "permafrost_bricks",
+			"red_sandstone_bricks", "sandstone_bricks", "cobblestone_bricks", "mossy_cobblestone_bricks", "blackstone_bricks", "diorite_bricks", "granite_bricks",
+			"andesite_bricks", "calcite_bricks", "dripstone_bricks", "raw_iron_bricks", "raw_gold_bricks", "raw_copper_bricks");
+    public static final List<String> leaves = List.of("blue_blossom", "lavender_blossom", "orange_blossom", "red_blossom", "yellow_blossom", "ancient");
+    public static final List<String> wood = List.of("blossom", "azalea", "ancient");
+
+	private static final DeferredRegister.Blocks BLOCKS = Registration.blocks(MODID);
+	private static final DeferredRegister.Items ITEMS = Registration.items(MODID);
+	private static final DeferredRegister<CreativeModeTab> CT = Registration.creativeModeTab(MODID);
+
+	public static final DeferredHolder<CreativeModeTab, CreativeModeTab> MCWQUARK_TAB = CT.register("tab", () -> CreativeModeTab.builder()
+	.icon(McwQuark::getIcon).title(Component.translatable(MODID + ".tab")).build());
+
+	public McwQuark(IEventBus bus)
+    {
+        super(bus);
+        List<String> rockClassic = List.of("soul_sandstone_bricks", "limestone_bricks", "jasper_bricks", "shale_bricks", "permafrost_bricks",
+				"red_sandstone_bricks", "sandstone_bricks", "cobblestone_bricks", "mossy_cobblestone_bricks", "blackstone_bricks", "diorite_bricks", "granite_bricks",
+				"andesite_bricks", "raw_iron_bricks", "raw_gold_bricks", "raw_copper_bricks");
+
+		Map<String, SoundType> rockSounds = new LinkedHashMap<>();
+		rockSounds.put("calcite_bricks", SoundType.CALCITE);
+		rockSounds.put("dripstone_bricks", SoundType.DRIPSTONE_BLOCK);
+
+		LOGGER.info("Macaw's Quark Loading...");
+		Registration.init(bus, BLOCKS, ITEMS, CT);
+
+		McwRegistry.setRegistriesWood(wood, BLOCKS, ITEMS, Registration.getAllModTypeWood());
+		McwRegistry.setRegistriesLeave(leaves, BLOCKS, ITEMS);
+		McwRegistry.setRegistriesStone(rockClassic, BLOCKS, ITEMS, Registration.getAllModTypeStone());
+		McwRegistry.setRegistriesStone(rockSounds, BLOCKS, ITEMS, Registration.getAllModTypeStone());
+
+		bus.addListener(this::clientSetup);
+		bus.addListener(this::commonSetup);
+		bus.addListener(this::dataSetup);
+		bus.addListener(this::tabSetup);
+		LOGGER.info("Macaw's Quark Is Charged !");
+	}
+
+    @Override
+    public void clientSetup(FMLClientSetupEvent event) {
+		event.enqueueWork(() -> {
+			APIRenderTypes.initAllWood(event, MODID, wood, Registration.getAllModTypeWood());
+			APIRenderTypes.initAllLeave(event, MODID, leaves);
+			APIRenderTypes.initAllWood(event, MODID, stone, Registration.getAllModTypeStone());
+		});
+    }
+
+    @Override
+    public void commonSetup(FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+            McwLootTables.addBlockAllWood(MODID, wood);
+            McwLootTables.addBlockHedges(MODID, leaves);
+            McwLootTables.addBlockAllStone(MODID, stone);
+        });
+    }
+
+    @Override
+    public void dataSetup(GatherDataEvent gatherDataEvent) {
+        DataGenerator generator = gatherDataEvent.getGenerator();
+
+        PackOutput output = generator.getPackOutput();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = gatherDataEvent.getLookupProvider();
+        ExistingFileHelper existingFileHelper = gatherDataEvent.getExistingFileHelper();
+
+        if(gatherDataEvent.includeServer())
+        {
+            McwBlockTags mcwBlockTags = new McwBlockTags(output, lookupProvider, MODID, existingFileHelper) {
+                @Override
+                protected void addTags(HolderLookup.@NotNull Provider provider) {
+					addAllMcwTagsWood(MODID, wood, Registration.getAllModTypeWood());
+					addAllMcwTagsLeave(MODID, leaves);
+					addAllMcwTagsStone(MODID, stone, Registration.getAllModTypeStone());
+                }
+            };
+
+            generator.addProvider(true, new Recipes(output, lookupProvider));
+            generator.addProvider(true, mcwBlockTags);
+            generator.addProvider(true, new McwItemTags(output, lookupProvider, mcwBlockTags.contentsGetter(), MODID, existingFileHelper) {
+                @Override
+                protected void addTags(HolderLookup.@NotNull Provider provider) {
+					addAllMcwTagsWood(MODID, wood, Registration.getAllModTypeWood());
+					addAllMcwTagsLeave(MODID, leaves);
+					addAllMcwTagsStone(MODID, stone, Registration.getAllModTypeStone());
+                }
+            });
+        }
+    }
+
+    @Override
+    public void tabSetup(BuildCreativeModeTabContentsEvent event) {
+        APICreativeTab.initAllWood(event, MODID, wood, MCWQUARK_TAB.get(), Registration.getAllModTypeWood());
+        APICreativeTab.initAllLeave(event, MODID, leaves, MCWQUARK_TAB.get());
+        APICreativeTab.initAllStone(event, MODID, stone, MCWQUARK_TAB.get(), Registration.getAllModTypeStone());
+    }
+
+	@Nonnull
+	private static ItemStack getIcon()
+	{
+		NewIconRandom.NewProperties propIcon = new NewIconRandom.NewProperties(
+				Finder.findBlock(MODID, "limestone_bricks_roof"),
+				Finder.findBlock(MODID, "orange_blossom_hedge"),
+				Finder.findBlock(MODID, "blossom_bookshelf"),
+				Finder.findBlock(MODID, "marble_bricks_bridge"),
+				Finder.findBlock(MODID, "azalea_shutter"),
+				Finder.findBlock(MODID, "blossom_beach_door"),
+				Finder.findBlock(MODID, "azalea_paper_trapdoor"),
+				Finder.findBlock(MODID, "blossom_planks_path"),
+				Finder.findBlock(MODID, "ancient_balcony"));
+
+		propIcon
+				.addType(ModType.BRIDGES)
+				.addType(ModType.ROOFS)
+				.addType(ModType.FENCES)
+				.addType(ModType.FURNITURES)
+				.addType(ModType.STAIRS)
+				.addType(ModType.WINDOWS)
+				.addType(ModType.DOORS)
+				.addType(ModType.TRAPDOORS)
+				.addType(ModType.PATHS);
+		Block icon = propIcon.buildIcon(ModType.BRIDGES, ModType.ROOFS, ModType.FENCES, ModType.FURNITURES,
+				ModType.STAIRS, ModType.WINDOWS, ModType.DOORS, ModType.TRAPDOORS, ModType.PATHS);
+		return new ItemStack(icon);
+	}
+}
---
+++ Quark/1.21.1-Neoforge/src/main/java/fr/samlegamer/mcwquark/Recipes.java
@@ -0,0 +1,176 @@
+package fr.samlegamer.mcwquark;
+
+import fr.addonslib.api.recipes.material.McwStoneMat;
+import fr.addonslib.api.recipes.material.McwWoodMat;
+import fr.samlegamer.addonslib.Finder;
+import fr.samlegamer.addonslib.generation.recipes.McwRecipes;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import org.jetbrains.annotations.NotNull;
+import org.violetmoon.quark.base.Quark;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class Recipes extends McwRecipes
+{
+    public Recipes(PackOutput p_248933_, CompletableFuture<HolderLookup.Provider> p_333797_) {
+        super(p_248933_, p_333797_);
+    }
+
+    @Override
+    protected void buildRecipes(@NotNull RecipeOutput consumer) {
+        registerAllMcwWood(consumer, McwQuark.MODID, Quark.MOD_ID, McwQuark.wood, wood());
+        registerMcwHedge(consumer, McwQuark.MODID, Quark.MOD_ID, McwQuark.leaves, leave());
+        registerAllMcwStone(consumer, McwQuark.MODID, Quark.MOD_ID, McwQuark.stone, stone());
+    }
+
+    private List<Block> leave()
+    {
+        List<Block> mats = new ArrayList<>();
+        for(String i : McwQuark.leaves)
+        {
+            mats.add(getBlock(i+"_leaves"));
+        }
+        return mats;
+    }
+
+    private List<McwStoneMat<Block>> stone()
+    {
+        List<McwStoneMat<Block>> mats = new ArrayList<>();
+        mats.add(new McwStoneMat<>(
+                getBlock("soul_sandstone_bricks"),
+                getBlock("soul_sandstone_bricks_wall"),
+                getBlock("soul_sandstone_bricks_slab"),
+                getBlock("soul_sandstone")
+        ));
+        mats.add(new McwStoneMat<>(
+                getBlock("limestone_bricks"),
+                getBlock("limestone_bricks_wall"),
+                getBlock("limestone_bricks_slab"),
+                getBlock("limestone")
+        ));
+        mats.add(new McwStoneMat<>(
+                getBlock("jasper_bricks"),
+                getBlock("jasper_bricks_wall"),
+                getBlock("jasper_bricks_slab"),
+                getBlock("jasper")
+        ));
+        mats.add(new McwStoneMat<>(
+                getBlock("shale_bricks"),
+                getBlock("shale_bricks_wall"),
+                getBlock("shale_bricks_slab"),
+                getBlock("shale")
+        ));
+        mats.add(new McwStoneMat<>(
+                getBlock("permafrost_bricks"),
+                getBlock("permafrost_bricks_wall"),
+                getBlock("permafrost_bricks_slab"),
+                getBlock("permafrost")
+        ));
+        mats.add(new McwStoneMat<>(
+                getBlock("red_sandstone_bricks"),
+                getBlock("red_sandstone_bricks_wall"),
+                getBlock("red_sandstone_bricks_slab"),
+                Blocks.SMOOTH_RED_SANDSTONE
+        ));
+        mats.add(new McwStoneMat<>(
+                getBlock("sandstone_bricks"),
+                getBlock("sandstone_bricks_wall"),
+                getBlock("sandstone_bricks_slab"),
+                Blocks.SMOOTH_SANDSTONE
+        ));
+        mats.add(new McwStoneMat<>(
+                getBlock("cobblestone_bricks"),
+                getBlock("cobblestone_bricks_wall"),
+                getBlock("cobblestone_bricks_slab"),
+                Blocks.COBBLESTONE
+        ));
+        mats.add(new McwStoneMat<>(
+                getBlock("mossy_cobblestone_bricks"),
+                getBlock("mossy_cobblestone_bricks_wall"),
+                getBlock("mossy_cobblestone_bricks_slab"),
+                Blocks.MOSSY_COBBLESTONE
+        ));
+        mats.add(new McwStoneMat<>(
+                getBlock("blackstone_bricks"),
+                getBlock("blackstone_bricks_wall"),
+                getBlock("blackstone_bricks_slab"),
+                Blocks.BLACKSTONE
+        ));
+        mats.add(new McwStoneMat<>(
+                getBlock("diorite_bricks"),
+                getBlock("diorite_bricks_wall"),
+                getBlock("diorite_bricks_slab"),
+                Blocks.DIORITE
+        ));
+        mats.add(new McwStoneMat<>(
+                getBlock("granite_bricks"),
+                getBlock("granite_bricks_wall"),
+                getBlock("granite_bricks_slab"),
+                Blocks.GRANITE
+        ));
+        mats.add(new McwStoneMat<>(
+                getBlock("andesite_bricks"),
+                getBlock("andesite_bricks_wall"),
+                getBlock("andesite_bricks_slab"),
+                Blocks.ANDESITE
+        ));
+        mats.add(new McwStoneMat<>(
+                getBlock("calcite_bricks"),
+                getBlock("calcite_bricks_wall"),
+                getBlock("calcite_bricks_slab"),
+                Blocks.CALCITE
+        ));
+        mats.add(new McwStoneMat<>(
+                getBlock("dripstone_bricks"),
+                getBlock("dripstone_bricks_wall"),
+                getBlock("dripstone_bricks_slab"),
+                Blocks.DRIPSTONE_BLOCK
+        ));
+        mats.add(new McwStoneMat<>(
+                getBlock("raw_iron_bricks"),
+                getBlock("raw_iron_bricks_wall"),
+                getBlock("raw_iron_bricks_slab"),
+                Blocks.RAW_IRON_BLOCK
+        ));
+        mats.add(new McwStoneMat<>(
+                getBlock("raw_gold_bricks"),
+                getBlock("raw_gold_bricks_wall"),
+                getBlock("raw_gold_bricks_slab"),
+                Blocks.RAW_GOLD_BLOCK
+        ));
+        mats.add(new McwStoneMat<>(
+                getBlock("raw_copper_bricks"),
+                getBlock("raw_copper_bricks_wall"),
+                getBlock("raw_copper_bricks_slab"),
+                Blocks.RAW_COPPER_BLOCK
+        ));
+        return mats;
+    }
+
+    private List<McwWoodMat<Block>> wood()
+    {
+        List<McwWoodMat<Block>> mats = new ArrayList<>();
+        for(String i : McwQuark.wood)
+        {
+            mats.add(new McwWoodMat<>(
+                    getBlock(i + "_log"),
+                    getBlock(i + "_planks"),
+                    getBlock("stripped_" + i + "_log"),
+                    getBlock(i + "_planks_slab"),
+                    getBlock(i + "_fence"),
+                    getBlock(i + "_trapdoor")
+            ));
+        }
+        return mats;
+    }
+
+    private Block getBlock(String name)
+    {
+        return Finder.findBlock(Quark.MOD_ID, name);
+    }
+}
