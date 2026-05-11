52734fde35c727f23e8ed08759b470389cdb1f20
Samlegamer <83614614+Samlegamer@users.noreply.github.com>
2026-05-11 20:37:07 +0200

    test implements in region un 1.18.2 forge

--- RegionsUnexplored/1.18.2/src/main/java/fr/samlegamer/mcwregionsunexplored/McwRegionsUnexplored.java
+++ RegionsUnexplored/1.18.2/src/main/java/fr/samlegamer/mcwregionsunexplored/McwRegionsUnexplored.java
@@ -1,11 +1,13 @@
 package fr.samlegamer.mcwregionsunexplored;
 
 import fr.addonslib.api.data.ModType;
+import fr.samlegamer.addonslib.RegistrationForge;
 import fr.samlegamer.addonslib.client.APIRenderTypes;
 import fr.samlegamer.addonslib.generation.loot_tables.McwLootTables;
 import fr.samlegamer.addonslib.generation.tags.McwBlockTags;
 import fr.samlegamer.addonslib.generation.tags.McwItemTags;
 import fr.samlegamer.addonslib.registry.McwRegistry;
+import fr.samlegamer.addonslib.tab.IconRandomForge;
 import fr.samlegamer.addonslib.util.McwMod;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.world.item.CreativeModeTab;
@@ -23,8 +25,6 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import fr.samlegamer.addonslib.Finder;
-import fr.samlegamer.addonslib.Registration;
-import fr.samlegamer.addonslib.tab.NewIconRandom;
 import org.jetbrains.annotations.NotNull;
 
 @Mod(McwRegionsUnexplored.MODID)
@@ -38,13 +38,13 @@ public class McwRegionsUnexplored extends McwMod
     "larch", "maple", "mauve", "palm", "pine", "redwood", "sculkwood", "willow", "alpha_oak", "pink_cherry", "red_cherry", "white_cherry",
     "red_maple", "orange_maple", "golden_larch", "dead_pine", "silver_birch");
 
-    private static final DeferredRegister<Block> block = Registration.blocks(MODID);
-    private static final DeferredRegister<Item> item = Registration.items(MODID);
+    private static final DeferredRegister<Block> block = RegistrationForge.blocks(MODID);
+    private static final DeferredRegister<Item> item = RegistrationForge.items(MODID);
 
     public static final CreativeModeTab MCWREGIONUNEXPLORED_TAB = new CreativeModeTab(MODID + ".tab") {
         @Override
         public @NotNull ItemStack makeIcon() {
-            NewIconRandom.NewProperties woodProperties = new NewIconRandom.NewProperties(
+            return IconRandomForge.buildIcon(
                     Finder.findBlock(MODID, "palm_roof"),
                     Finder.findBlock(MODID, "willow_picket_fence"),
                     Finder.findBlock(MODID, "redwood_wardrobe"),
@@ -53,29 +53,16 @@ public class McwRegionsUnexplored extends McwMod
                     Finder.findBlock(MODID, "larch_japanese_door"),
                     Finder.findBlock(MODID, "maple_glass_trapdoor"),
                     Finder.findBlock(MODID, "baobab_planks_path"),
-                    Finder.findBlock(MODID, "pine_loft_stairs")
-            );
-            woodProperties
-                    .addType(ModType.ROOFS)
-                    .addType(ModType.FENCES)
-                    .addType(ModType.FURNITURES)
-                    .addType(ModType.BRIDGES)
-                    .addType(ModType.WINDOWS)
-                    .addType(ModType.DOORS)
-                    .addType(ModType.TRAPDOORS)
-                    .addType(ModType.PATHS)
-                    .addType(ModType.STAIRS);
-            Block icon = woodProperties.buildIcon(ModType.ROOFS, ModType.FENCES, ModType.FURNITURES, ModType.BRIDGES, ModType.WINDOWS,
-                    ModType.DOORS, ModType.TRAPDOORS, ModType.PATHS, ModType.STAIRS);
-            return new ItemStack(icon);
+                    Finder.findBlock(MODID, "pine_loft_stairs"),
+                    ModType.getAllModTypeWood());
         }
     };
 
     public McwRegionsUnexplored()
     {
         LOGGER.info("Macaw's Regions Unexplored Loading...");
-        Registration.init(block, item);
-        McwRegistry.setRegistriesWood(WOOD, block, item, MCWREGIONUNEXPLORED_TAB, Registration.getAllModTypeWood());
+        RegistrationForge.init(block, item);
+        McwRegistry.setRegistriesWood(WOOD, block, item, MCWREGIONUNEXPLORED_TAB, ModType.getAllModTypeWood());
         McwRegistry.setRegistriesLeave(LEAVES, block, item, MCWREGIONUNEXPLORED_TAB);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
@@ -85,15 +72,15 @@ public class McwRegionsUnexplored extends McwMod
 
     @Override
     public void clientSetup(FMLClientSetupEvent fmlClientSetupEvent) {
-        APIRenderTypes.initAllWood(fmlClientSetupEvent, MODID, WOOD, Registration.getAllModTypeWood());
+        APIRenderTypes.initAllWood(fmlClientSetupEvent, MODID, WOOD, ModType.getAllModTypeWood());
         APIRenderTypes.initAllLeave(fmlClientSetupEvent, MODID, LEAVES);
     }
 
     @Override
     public void commonSetup(FMLCommonSetupEvent fmlCommonSetupEvent) {
         fmlCommonSetupEvent.enqueueWork(() -> {
-            McwLootTables.addBlockAllWood(MODID, WOOD);
-            McwLootTables.addBlockHedges(MODID, LEAVES);
+            McwLootTables.LOOT_TABLE_UTILS.addBlockAllWood(MODID, WOOD);
+            McwLootTables.LOOT_TABLE_UTILS.addBlockHedges(MODID, LEAVES);
         });
     }
 
@@ -106,7 +93,7 @@ public class McwRegionsUnexplored extends McwMod
             McwBlockTags mcwBlockTags = new McwBlockTags(generator, MODID, existingFileHelper) {
                 @Override
                 protected void addTags() {
-                    addAllMcwTagsWood(MODID, WOOD, Registration.getAllModTypeWood());
+                    addAllMcwTagsWood(MODID, WOOD, ModType.getAllModTypeWood());
                     addAllMcwTagsLeave(MODID, LEAVES);
                 }
             };
@@ -115,7 +102,7 @@ public class McwRegionsUnexplored extends McwMod
             generator.addProvider(new McwItemTags(generator, mcwBlockTags, MODID, existingFileHelper) {
                 @Override
                 protected void addTags() {
-                    addAllMcwTagsWood(MODID, WOOD, Registration.getAllModTypeWood());
+                    addAllMcwTagsWood(MODID, WOOD, ModType.getAllModTypeWood());
                     addAllMcwTagsLeave(MODID, LEAVES);
                 }
             });
