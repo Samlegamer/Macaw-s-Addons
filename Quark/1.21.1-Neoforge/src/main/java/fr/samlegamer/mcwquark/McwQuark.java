114f81821378fdbf2ef7994abfd2e63a0c6c203d
Samlegamer <83614614+Samlegamer@users.noreply.github.com>
2026-06-02 16:21:44 +0200

    Quark Neoforge 1.21.1 for v1.6 and fix crash furniture

--- Quark/1.21.1-Neoforge/src/main/java/fr/samlegamer/mcwquark/McwQuark.java
+++ Quark/1.21.1-Neoforge/src/main/java/fr/samlegamer/mcwquark/McwQuark.java
@@ -1,13 +1,14 @@
 package fr.samlegamer.mcwquark;
 
 import fr.addonslib.api.data.ModType;
-import fr.samlegamer.addonslib.Registration;
+import fr.samlegamer.addonslib.RegistrationNeoForge;
 import fr.samlegamer.addonslib.client.APIRenderTypes;
 import fr.samlegamer.addonslib.generation.loot_tables.McwLootTables;
 import fr.samlegamer.addonslib.generation.tags.McwBlockTags;
 import fr.samlegamer.addonslib.generation.tags.McwItemTags;
 import fr.samlegamer.addonslib.registry.McwRegistry;
 import fr.samlegamer.addonslib.tab.APICreativeTab;
+import fr.samlegamer.addonslib.util.McwCommon;
 import fr.samlegamer.addonslib.util.McwMod;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
@@ -15,7 +16,6 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -27,13 +27,14 @@ import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+import net.neoforged.neoforge.event.BlockEntityTypeAddBlocksEvent;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import fr.samlegamer.addonslib.Finder;
-import fr.samlegamer.addonslib.tab.NewIconRandom;
+import fr.samlegamer.addonslib.tab.IconRandomNeoForge;
 import org.jetbrains.annotations.NotNull;
 import javax.annotation.Nonnull;
 
@@ -49,9 +50,9 @@ public class McwQuark extends McwMod
     public static final List<String> leaves = List.of("blue_blossom", "lavender_blossom", "orange_blossom", "red_blossom", "yellow_blossom", "ancient");
     public static final List<String> wood = List.of("blossom", "azalea", "ancient");
 
-	private static final DeferredRegister.Blocks BLOCKS = Registration.blocks(MODID);
-	private static final DeferredRegister.Items ITEMS = Registration.items(MODID);
-	private static final DeferredRegister<CreativeModeTab> CT = Registration.creativeModeTab(MODID);
+	private static final DeferredRegister.Blocks BLOCKS = RegistrationNeoForge.blocks(MODID);
+	private static final DeferredRegister.Items ITEMS = RegistrationNeoForge.items(MODID);
+	private static final DeferredRegister<CreativeModeTab> CT = RegistrationNeoForge.creativeModeTab(MODID);
 
 	public static final DeferredHolder<CreativeModeTab, CreativeModeTab> MCWQUARK_TAB = CT.register("tab", () -> CreativeModeTab.builder()
 	.icon(McwQuark::getIcon).title(Component.translatable(MODID + ".tab")).build());
@@ -68,35 +69,36 @@ public class McwQuark extends McwMod
 		rockSounds.put("dripstone_bricks", SoundType.DRIPSTONE_BLOCK);
 
 		LOGGER.info("Macaw's Quark Loading...");
-		Registration.init(bus, BLOCKS, ITEMS, CT);
+		RegistrationNeoForge.init(bus, BLOCKS, ITEMS, CT);
 
-		McwRegistry.setRegistriesWood(wood, BLOCKS, ITEMS, Registration.getAllModTypeWood());
+		McwRegistry.setRegistriesWood(wood, BLOCKS, ITEMS, ModType.getAllModTypeWood());
 		McwRegistry.setRegistriesLeave(leaves, BLOCKS, ITEMS);
-		McwRegistry.setRegistriesStone(rockClassic, BLOCKS, ITEMS, Registration.getAllModTypeStone());
-		McwRegistry.setRegistriesStone(rockSounds, BLOCKS, ITEMS, Registration.getAllModTypeStone());
+		McwRegistry.setRegistriesStone(rockClassic, BLOCKS, ITEMS, ModType.getAllModTypeStone());
+		McwRegistry.setRegistriesStone(rockSounds, BLOCKS, ITEMS, ModType.getAllModTypeStone());
 
 		bus.addListener(this::clientSetup);
 		bus.addListener(this::commonSetup);
 		bus.addListener(this::dataSetup);
 		bus.addListener(this::tabSetup);
+		bus.addListener(this::addToFurnitureStorage);
 		LOGGER.info("Macaw's Quark Is Charged !");
 	}
 
     @Override
     public void clientSetup(FMLClientSetupEvent event) {
 		event.enqueueWork(() -> {
-			APIRenderTypes.initAllWood(event, MODID, wood, Registration.getAllModTypeWood());
+			APIRenderTypes.initAllWood(event, MODID, wood, ModType.getAllModTypeWood());
 			APIRenderTypes.initAllLeave(event, MODID, leaves);
-			APIRenderTypes.initAllWood(event, MODID, stone, Registration.getAllModTypeStone());
+			APIRenderTypes.initAllWood(event, MODID, stone, ModType.getAllModTypeStone());
 		});
     }
 
     @Override
     public void commonSetup(FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
-            McwLootTables.addBlockAllWood(MODID, wood);
-            McwLootTables.addBlockHedges(MODID, leaves);
-            McwLootTables.addBlockAllStone(MODID, stone);
+            McwLootTables.LOOT_TABLE_UTILS.addBlockAllWood(MODID, wood);
+            McwLootTables.LOOT_TABLE_UTILS.addBlockHedges(MODID, leaves);
+            McwLootTables.LOOT_TABLE_UTILS.addBlockAllStone(MODID, stone);
         });
     }
 
@@ -113,9 +115,9 @@ public class McwQuark extends McwMod
             McwBlockTags mcwBlockTags = new McwBlockTags(output, lookupProvider, MODID, existingFileHelper) {
                 @Override
                 protected void addTags(HolderLookup.@NotNull Provider provider) {
-					addAllMcwTagsWood(MODID, wood, Registration.getAllModTypeWood());
+					addAllMcwTagsWood(MODID, wood, ModType.getAllModTypeWood());
 					addAllMcwTagsLeave(MODID, leaves);
-					addAllMcwTagsStone(MODID, stone, Registration.getAllModTypeStone());
+					addAllMcwTagsStone(MODID, stone, ModType.getAllModTypeStone());
                 }
             };
 
@@ -124,9 +126,9 @@ public class McwQuark extends McwMod
             generator.addProvider(true, new McwItemTags(output, lookupProvider, mcwBlockTags.contentsGetter(), MODID, existingFileHelper) {
                 @Override
                 protected void addTags(HolderLookup.@NotNull Provider provider) {
-					addAllMcwTagsWood(MODID, wood, Registration.getAllModTypeWood());
+					addAllMcwTagsWood(MODID, wood, ModType.getAllModTypeWood());
 					addAllMcwTagsLeave(MODID, leaves);
-					addAllMcwTagsStone(MODID, stone, Registration.getAllModTypeStone());
+					addAllMcwTagsStone(MODID, stone, ModType.getAllModTypeStone());
                 }
             });
         }
@@ -134,15 +136,19 @@ public class McwQuark extends McwMod
 
     @Override
     public void tabSetup(BuildCreativeModeTabContentsEvent event) {
-        APICreativeTab.initAllWood(event, MODID, wood, MCWQUARK_TAB.get(), Registration.getAllModTypeWood());
+        APICreativeTab.initAllWood(event, MODID, wood, MCWQUARK_TAB.get(), ModType.getAllModTypeWood());
         APICreativeTab.initAllLeave(event, MODID, leaves, MCWQUARK_TAB.get());
-        APICreativeTab.initAllStone(event, MODID, stone, MCWQUARK_TAB.get(), Registration.getAllModTypeStone());
+        APICreativeTab.initAllStone(event, MODID, stone, MCWQUARK_TAB.get(), ModType.getAllModTypeStone());
     }
+
+	private void addToFurnitureStorage(BlockEntityTypeAddBlocksEvent event) {
+		McwCommon.addCompatibleBlocksToFurnitureStorage(event, MODID, wood);
+	}
 
 	@Nonnull
 	private static ItemStack getIcon()
 	{
-		NewIconRandom.NewProperties propIcon = new NewIconRandom.NewProperties(
+		return IconRandomNeoForge.buildIcon(
 				Finder.findBlock(MODID, "limestone_bricks_roof"),
 				Finder.findBlock(MODID, "orange_blossom_hedge"),
 				Finder.findBlock(MODID, "blossom_bookshelf"),
@@ -151,20 +157,7 @@ public class McwQuark extends McwMod
 				Finder.findBlock(MODID, "blossom_beach_door"),
 				Finder.findBlock(MODID, "azalea_paper_trapdoor"),
 				Finder.findBlock(MODID, "blossom_planks_path"),
-				Finder.findBlock(MODID, "ancient_balcony"));
-
-		propIcon
-				.addType(ModType.BRIDGES)
-				.addType(ModType.ROOFS)
-				.addType(ModType.FENCES)
-				.addType(ModType.FURNITURES)
-				.addType(ModType.STAIRS)
-				.addType(ModType.WINDOWS)
-				.addType(ModType.DOORS)
-				.addType(ModType.TRAPDOORS)
-				.addType(ModType.PATHS);
-		Block icon = propIcon.buildIcon(ModType.BRIDGES, ModType.ROOFS, ModType.FENCES, ModType.FURNITURES,
-				ModType.STAIRS, ModType.WINDOWS, ModType.DOORS, ModType.TRAPDOORS, ModType.PATHS);
-		return new ItemStack(icon);
+				Finder.findBlock(MODID, "ancient_balcony"),
+				ModType.getAllModTypeWood());
 	}
 }
